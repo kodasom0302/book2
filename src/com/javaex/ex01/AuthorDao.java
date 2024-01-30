33f7cf3ceca3eb734bdb27b055f0efee05b23966
dasom <dasom589@gmail.com>
2024-01-30 12:19:05 +0900

    2024-01-30 3교시

--- src/com/javaex/ex01/AuthorDao.java
+++ src/com/javaex/ex01/AuthorDao.java
@@ -1,12 +1,30 @@
 package com.javaex.ex01;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AuthorDao {
 	
-	public void authorInsert(String name, String desc) {
-		//book_db 데이터 베이스에 접속
-		//아이디 : book, 비밀번호 : book
-		//author 테이블에 작가를 추가(=insert 인서트) 이름/설명
+	//필드
+	//생성자 - 디폴트
+	//메소드 gs
+	//메소드 - 일반
+	//작가 리스트
+	public List<AuthorVo> authorList() {
+		//리스트 만들기
+		//db에서 데이터 가져오기
+		//리스트에 추가
+		//리스트 주소 전달하기
 		
+		//리스트 준비
+		List<AuthorVo> authorList=new ArrayList<AuthorVo>();
+		
+		//db에서 데이터 가져오기
 		// 0. import java.sql.*;
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -20,11 +38,26 @@ public class AuthorDao {
 			conn = DriverManager.getConnection(url, "book", "book");
 			
 		// 3. SQL문 준비 / 바인딩 / 실행
-			String query="";
-			query+=" insert into author ";
-			query+=" values (null, '박명수', '개그맨') ";
+			String query = "";
+			query += " select   author_id, ";
+			query += " 			author_name, ";
+			query += " 			author_desc ";
+			query += " from author ";
+			
+			pstmt=conn.prepareStatement(query);
+			
+			rs=pstmt.executeQuery();
 			
 		// 4.결과처리
+			while (rs.next()) {
+				int no=rs.getInt("author_id");
+				String name=rs.getString("author_name");
+				String desc=rs.getString("author_desc");
+				
+				AuthorVo authorVo=new AuthorVo(no, name, desc);
+				
+				authorList.add(authorVo);
+			}
 		} catch (ClassNotFoundException e) {
 		System.out.println("error: 드라이버 로딩 실패 - " + e);
 		} catch (SQLException e) {
@@ -45,6 +78,112 @@ public class AuthorDao {
 		System.out.println("error:" + e);
 		}
 		}
-	}
+		
+		return authorList;
+		
+	}//authorList()
+	
+	//작가 삭제
+	public void authorDelete(int no) {
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+		// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+		// 2. Connection 얻어오기
+			String url = "jdbc:mysql://localhost:3306/book_db";
+			conn = DriverManager.getConnection(url, "book", "book");
+			
+		// 3. SQL문 준비 / 바인딩 / 실행
+			String query="";
+			query+=" delete from author ";
+			query+=" where author_id=? ";
+			
+			pstmt=conn.prepareStatement(query);
+			pstmt.setInt(1, no);
+			
+			int count=pstmt.executeUpdate();
+			
+		// 4.결과처리
+			System.out.println(count+"건 삭제 되었습니다.");
+			
+		} catch (ClassNotFoundException e) {
+		System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		System.out.println("error:" + e);
+		} finally {
+		// 5. 자원정리
+		try {
+		if (rs != null) {
+		rs.close();
+		}
+		if (pstmt != null) {
+		pstmt.close();
+		}
+		if (conn != null) {
+		conn.close();
+		}
+		} catch (SQLException e) {
+		System.out.println("error:" + e);
+		}
+		}
+	}//authorDelete()
+	
+	//작가 등록
+	public void authorInsert(String name, String desc) {
+		//book_db 데이터 베이스에 접속
+		//아이디 : book, 비밀번호 : book
+		//author 테이블에 작가를 추가(=insert 인서트) 이름/설명
+		
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+		// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+		// 2. Connection 얻어오기
+			String url = "jdbc:mysql://localhost:3306/book_db";
+			conn = DriverManager.getConnection(url, "book", "book");
+			
+		// 3. SQL문 준비 / 바인딩 / 실행
+			String query="";
+			query+=" insert into author ";
+			query+=" values (null, ?, ?) ";
+			
+			pstmt=conn.prepareStatement(query);
+			pstmt.setString(1, name);
+			pstmt.setString(2, desc);
+			
+			int count=pstmt.executeUpdate();
+			
+		// 4.결과처리
+			System.out.println(count+"건 입력 되었습니다.");
+			
+		} catch (ClassNotFoundException e) {
+		System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		System.out.println("error:" + e);
+		} finally {
+		// 5. 자원정리
+		try {
+		if (rs != null) {
+		rs.close();
+		}
+		if (pstmt != null) {
+		pstmt.close();
+		}
+		if (conn != null) {
+		conn.close();
+		}
+		} catch (SQLException e) {
+		System.out.println("error:" + e);
+		}
+		}
+	}//authorInsert()
 
 }
--- src/com/javaex/ex01/BookApp.java
+++ src/com/javaex/ex01/BookApp.java
@@ -1,9 +1,37 @@
 package com.javaex.ex01;
 
+import java.util.List;
+
 public class BookApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		AuthorDao authorDao=new AuthorDao();
+		
+		//작가 등록
+		//authorDao.authorInsert("이효리", "제주도민");
+		
+		//작가 삭제
+		//authorDao.authorDelete(14);
+		
+		//작가 리스트
+		List<AuthorVo> authorList=authorDao.authorList();
+		for (AuthorVo authorVo:authorList) {
+			int id=authorVo.getAuthorId();
+			String name=authorVo.getAuthorName();
+			String desc=authorVo.getAuthorDesc();
+			System.out.println(id+",	"+name+",	"+desc);
+		}
+		/*
+		for (int i=0; i<authorList.size(); i++) {
+			int id=authorList.get(i).getAuthorId();
+			String name=authorList.get(i).getAuthorName();
+			String desc=authorList.get(i).getAuthorDesc();
+			System.out.println(id+",	"+name+",	"+desc);
+		}
+		
+		System.out.println(authorList.size()+"명의 작가가 등록되어 있습니다.");
+		*/
 
 	}
 
